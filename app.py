25b8d9d2208d5d5145e6ed837d605577e1802128
XiangQinxi <xiangqinxi@outlook.com>
2023-04-25 20:52:48 +0800

    main

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+
 st.title("湖南省邵阳市第七中学 2201班")
 
 st.caption('此文档为七中2201班所编辑', help="目的在于记录2201班所发生过的事，以留做纪念")
+
+st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
